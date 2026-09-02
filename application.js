0cf9d9a6e29f6fa3ab2823c9b7d4cf51dc5fe4e8
readercar <155518565+readercar@users.noreply.github.com>
2026-09-02 11:58:44 +0900

    Update playable web build

--- application.js
+++ application.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.json?v=130dcf76a079';
+          this.settingsPath = 'src/settings.json?v=9bf14ebd4b28';
           this.showFPS = false;
         }
         _createClass(Application, [{
