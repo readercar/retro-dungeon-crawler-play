ec892ea853a10a9136c26e852fa6ae7d397f2c95
readercar <155518565+readercar@users.noreply.github.com>
2026-09-04 04:55:00 +0900

    Update playable web build

--- application.js
+++ application.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.json?v=20b2607634e0';
+          this.settingsPath = 'src/settings.json?v=a6788ba1c103';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-System.register(["./application.js?v=20b2607634e0"], function (_export, _context) {
+System.register(["./application.js?v=a6788ba1c103"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
