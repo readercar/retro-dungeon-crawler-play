a07a8ca8f98d013cdba60efb6be0bdc8bc99084f
readercar <155518565+readercar@users.noreply.github.com>
2026-09-07 05:02:10 +0900

    Update playable web build

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-System.register(["./application.js?v=e92ca2ee5e5f"], function (_export, _context) {
+System.register(["./application.js?v=ce8a3b37452a"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
